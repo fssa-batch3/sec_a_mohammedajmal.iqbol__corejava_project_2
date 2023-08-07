2c0fdb0632d9e5b63656e813273ab0d100541b8a
Ajmal112 <118426017+Ajmal112@users.noreply.github.com>
2023-08-07 17:02:31 +0530

    complete crud MySQL queries for milestone 1 and 2 added

--- sql-queries.sql
+++ sql-queries.sql
@@ -1,10 +1,66 @@
+USE project;
+SHOW tables;
 
-use project;
+ <--MILESTONE-1 COURSE USER DB CRUD QUERIES STARTED -->
+  <-- CREATE USER -->
 
+CREATE TABLE freshstocks (
+    userID INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    gender VARCHAR(10),
+    mobile_number VARCHAR(20),
+    date_of_birth DATE,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    is_deleted TINYINT(1) DEFAULT 0
+);
 
+  <-- READ USER BY USER BY USER EMAIL -->
 
--- INSERT INTO user (email,username,password) VALUES ("freekyajmal@gmail.com","Muthu","Ajmal112");
+SELECT * FROM freshstocks WHERE email = "freekyajmal@gmail.com";
+ 
+ 
+   <-- UPDATE USER BY USER EMAIL -->
+   
+UPDATE freshstocks SET gender = "MALE", mobile_number = "9500320194", date_of_birth = "2004-12-26" WHERE email = "freekyajmal@gmail.com";
+   
+   
+      <-- DELETE USER BY USER EMAIL -->
+      
+      
+UPDATE freshstocks SET is_deleted = 1 WHERE email = "freekyajmal@gmail.com";
+      
 
-SELECT * FROM user;
+  <--MILESTONE-2 COURSE MODULE DB CRUD QUERIES STARTED -->
+      
+        <-- CREATE COURSE -->
 
-DELETE FROM user;
+CREATE TABLE course (
+    courseID INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    cover_image LONGTEXT NOT NULL,
+    timing VARCHAR(20),
+    language VARCHAR(50) NOT NULL,
+    marked_price DECIMAL(10, 2) NOT NULL,
+    selling_price DECIMAL(10, 2) NOT NULL,
+    instructor_name VARCHAR(100),
+    company_name VARCHAR(100),
+    company_category VARCHAR(100),
+    top_skills VARCHAR(255),
+    is_deleted INT
+);
+
+  <-- READ COURSE FROM COURSE TABLE BY COURSE NAME -->
+
+ SELECT * FROM course WHERE name = "forex course";
+ 
+ 
+   <-- UPDATE COURSE BY COURSE NAME -->
+   
+UPDATE course SET cover_image="https://example.com", timing="40hrs", language="English", marked_price="5000", selling_price="1500", 
+instructor_name="Ajmal", company_name="freshstocks", company_category="Finance and banking", top_skills="smart money concepts" WHERE name = "forex course";
+   
+   <-- DELETE COURSE BY COURSE NAME -->
+      
+UPDATE course SET is_deleted = 1 WHERE name = "forex course";
+      
