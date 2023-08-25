02199f3ddba32c8a1a5e2e3ba06057a5ec4f5430
Ajmal112 <118426017+Ajmal112@users.noreply.github.com>
2023-08-25 09:38:30 +0530

    System .env variables added and established the connection

--- src/main/java/com/fssa/freshstocks/utils/ConnectionUtil.java
+++ src/main/java/com/fssa/freshstocks/utils/ConnectionUtil.java
@@ -1,7 +1,5 @@
 package com.fssa.freshstocks.utils;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -21,17 +19,21 @@ public class ConnectionUtil {
         final String dbUrl;
         final String dbUser;
         final String dbPassword;
-
-        if (System.getenv("CI") != null) {
-            dbUrl = System.getenv("DB_URL");
-            dbUser = System.getenv("DB_USER");
-            dbPassword = System.getenv("DB_PASSWORD");
-        } else {
-            Dotenv env = Dotenv.load();
-            dbUrl = env.get("DB_URL");
-            dbUser = env.get("DB_USER");
-            dbPassword = env.get("DB_PASSWORD");
-        }
+        
+      dbUrl = System.getenv("DB_URL");
+      dbUser = System.getenv("DB_USER");
+      dbPassword = System.getenv("DB_PASSWORD");
+//
+//        if (System.getenv("CI") != null) {
+//            dbUrl = System.getenv("DB_URL");
+//            dbUser = System.getenv("DB_USER");
+//            dbPassword = System.getenv("DB_PASSWORD");
+//        } else {
+//            Dotenv env = Dotenv.load();
+//            dbUrl = env.get("DB_URL");
+//            dbUser = env.get("DB_USER");
+//            dbPassword = env.get("DB_PASSWORD");
+//        }
         return DriverManager.getConnection(dbUrl, dbUser, dbPassword);
     }
 
